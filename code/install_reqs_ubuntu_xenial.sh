7b2239446b62a73300684eff09599306f50d18b5
Jeff Henrikson <jeff@groovescale.com>
2019-03-28 14:20:07 -0700

    fix: ubuntu package versions

--- code/install_reqs_ubuntu_xenial.sh
+++ code/install_reqs_ubuntu_xenial.sh
@@ -23,13 +23,13 @@ sudo apt-get install -y --no-install-recommends nvidia-410
 
 # Install development and runtime libraries (~4GB)
 sudo apt-get install -y --no-install-recommends \
-    cuda-10-0 \
-    libcudnn7=7.4.1.5-1+cuda10.0  \
-    libcudnn7-dev=7.4.1.5-1+cuda10.0
+    cuda-10-1 \
+    libcudnn7=7.5.0.56-1+cuda10.1  \
+    libcudnn7-dev=7.5.0.56-1+cuda10.1
 
 
 # Install TensorRT. Requires that libcudnn7 is installed above.
 sudo apt-get update && \
         sudo apt-get install -y nvinfer-runtime-trt-repo-ubuntu1604-5.0.2-ga-cuda10.0 \
         && sudo apt-get update \
-        && sudo apt-get install -y --no-install-recommends libnvinfer-dev=5.0.2-1+cuda10.0
+        && sudo apt-get install -y --no-install-recommends libnvinfer-dev=5.1.2-1+cuda10.1
