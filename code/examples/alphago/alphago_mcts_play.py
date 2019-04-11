7db6fb4a09e2596fa954aac48eb5c8f91ad9934d
Jeff Henrikson <jeff@groovescale.com>
2019-04-10 18:48:50 -0700

    fix: paths

--- code/examples/alphago/alphago_mcts_play.py
+++ code/examples/alphago/alphago_mcts_play.py
@@ -3,9 +3,13 @@ from dlgo.agent import load_prediction_agent, load_policy_agent, AlphaGoMCTS
 from dlgo.rl import load_value_agent
 import h5py
 
-fast_policy = load_prediction_agent(h5py.File('alphago_sl_policy.h5', 'r'))
-strong_policy = load_policy_agent(h5py.File('alphago_rl_policy.h5', 'r'))
-value = load_value_agent(h5py.File('alphago_value.h5', 'r'))
+import os
+
+adirCode=os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+fast_policy = load_prediction_agent(h5py.File(os.path.join(adirCode, 'alphago_sl_policy.h5'), 'r'))
+strong_policy = load_policy_agent(h5py.File(os.path.join(adirCode, 'alphago_rl_policy.h5'), 'r'))
+value = load_value_agent(h5py.File(os.path.join(adirCode, 'alphago_value.h5'), 'r'))
 
 alphago = AlphaGoMCTS(strong_policy, fast_policy, value)
 # end::run_alphago[]
