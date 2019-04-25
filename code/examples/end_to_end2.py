6142779c8b8293fa2c9806da0f09317fefb18a28
Jeff Henrikson <jeff@groovescale.com>
2019-04-24 22:23:15 -0700

    troubleshoot: waitasec, why does this work when the webapp doesn't?

--- code/examples/end_to_end2.py
+++ code/examples/end_to_end2.py
@@ -23,23 +23,24 @@ encoder = SevenPlaneEncoder((go_board_rows, go_board_cols))
 processor = GoDataProcessor(encoder=encoder.name(),
                             data_directory=os.path.join(adirCode,"data"))
 
-X, y = processor.load_go_data(num_samples=128)
-# end::e2e_processor[]
+if False:
+    X, y = processor.load_go_data(num_samples=128)
+    # end::e2e_processor[]
 
-# tag::e2e_model[]
-input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
-model = Sequential()
-network_layers = large.layers(input_shape)
-for layer in network_layers:
-    model.add(layer)
-model.add(Dense(nb_classes, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
+    # tag::e2e_model[]
+    input_shape = (encoder.num_planes, go_board_rows, go_board_cols)
+    model = Sequential()
+    network_layers = large.layers(input_shape)
+    for layer in network_layers:
+        model.add(layer)
+    model.add(Dense(nb_classes, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=['accuracy'])
 
-model.fit(X, y, batch_size=128, epochs=1, verbose=1)
-# end::e2e_model[]
+    model.fit(X, y, batch_size=128, epochs=1, verbose=1)
+    # end::e2e_model[]
 
-# tag::e2e_agent[]
-deep_learning_bot = DeepLearningAgent(model, encoder)
+    # tag::e2e_agent[]
+    deep_learning_bot = DeepLearningAgent(model, encoder)
 
 #how would you like to play?
 
@@ -53,20 +54,20 @@ board_size = 19
 row=3
 col=3
 game_state0 = goboard.GameState.new_game(board_size)
-print("prediction 0: {}".format(model.predict(np.array([encoder.encode(game_state0)]))))
+#print("prediction 0: {}".format(model.predict(np.array([encoder.encode(game_state0)]))))
 
 move1 = goboard.Move.play(Point(row, col))
 game_state1 = game_state0.apply_move(move1)
 
-print("prediction 1: {}".format(model.predict(np.array([encoder.encode(game_state1)]))))
+#print("prediction 1: {}".format(model.predict(np.array([encoder.encode(game_state1)]))))
 
-
-with h5py.File(os.path.join(rdirOut,"deep_bot.h5"), 'w') as file_agent_out:
-    deep_learning_bot.serialize(file_agent_out)
+if False:
+    with h5py.File(os.path.join(rdirOut,"deep_bot.h5"), 'w') as file_agent_out:
+        deep_learning_bot.serialize(file_agent_out)
 # end::e2e_agent[]
 
 # tag::e2e_load_agent[]
-model_file = h5py.File(os.path.join(rdirOut,"deep_bot.h5"), "r")
+model_file = h5py.File("../data/2019-04-24/deep_bot.h5", "r")
 bot_from_file = load_prediction_agent(model_file)
 
 print("successfully round tripped {}".format(os.path.join(rdirOut,"deep_bot.h5")))
